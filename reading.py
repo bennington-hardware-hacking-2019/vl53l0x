d612c6a36f11b0723b1029252561b5f6ee9b8824
Hoanh An <hoanhan@bennington.edu>
2019-04-21 19:58:54 +0000

    add sample reading

---
+++ reading.py
@@ -0,0 +1,21 @@
+#
+# reading.py shows vl53l0x tof reading on ht16k33 7-segment display
+#
+
+import time
+
+from vl53l0x.api import VL53L0X
+from ht16k33.api import HT16K33
+
+if __name__== "__main__":
+    tof = VL53L0X()
+    led = HT16K33()
+
+    tof.setup()
+    led.setup()
+
+    rest = 0.001
+    while True:
+        distance = tof.measure()
+        led.display(distance)
+        time.sleep(rest)
