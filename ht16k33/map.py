edacb53b48436b938aae2ed8b5ef141d0cff01ab
Hoanh An <hoanhan@bennington.edu>
2019-04-21 22:05:32 -0400

    add a map of string number and hex value

---
+++ ht16k33/map.py
@@ -0,0 +1,26 @@
+#
+# map.py contains the map of string number to its coressponding hex value
+#
+
+num_map = {
+        "0" : 0x3F,
+        "0.": 0xBF,
+        "1" : 0x06,
+        "1.": 0x86,
+        "2" : 0x5B,
+        "2.": 0xDB,
+        "3" : 0x4F,
+        "3.": 0xCF,
+        "4" : 0x66,
+        "4.": 0xE6,
+        "5" : 0x6D,
+        "5.": 0xED,
+        "6" : 0x7D,
+        "6.": 0xFD,
+        "7" : 0x07,
+        "7.": 0xA7,
+        "8" : 0x7F,
+        "8.": 0xFF,
+        "9" : 0x6F,
+        "9.": 0xEF
+}
