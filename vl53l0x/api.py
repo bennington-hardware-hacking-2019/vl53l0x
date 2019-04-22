a6690309948d17e77d9f1f6ab93fdd66fa6d6735
Hoanh An <hoanhan@bennington.edu>
2019-04-21 22:21:40 -0400

    update api comments

--- vl53l0x/api.py
+++ vl53l0x/api.py
@@ -23,17 +23,20 @@ class VL53L0X(object):
         self.measurement = 0
 
     def setup(self):
+        """configure the device when it first starts up"""
         self.data_init()
         self.static_init()
         self.perform_ref_calibration()
         self.perform_ref_spad_management()
 
     def measure(self):
+        """start measuring and return its data"""
         self.perform_ref_signal_measurement()
 
         return self.measurement
 
     def data_init(self):
+        """data initialization"""
         # set i2c standard mode
         self.write_byte(0x88, 0x00)
 
@@ -54,6 +57,7 @@ class VL53L0X(object):
         self.write_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG, 0xFF)
 
     def static_init(self):
+        """static initialization"""
         self.write_byte(0xFF, 0x01)
         self.read_byte(0x84)
         self.write_byte(0xFF, 0x00)
@@ -62,6 +66,7 @@ class VL53L0X(object):
         self.static_seq_config = self.read_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG)
 
     def perform_ref_calibration(self):
+        """perform ref calibration, including vhv and phase one"""
         self.perform_vhv_calibration()
         self.perform_phase_calibration()
 
@@ -69,6 +74,7 @@ class VL53L0X(object):
         self.write_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG, self.static_seq_config)
 
     def perform_vhv_calibration(self):
+        """perform vhv calibration"""
         # run vhv
         self.write_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG, 0x01)
 
@@ -81,6 +87,7 @@ class VL53L0X(object):
         self.write_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG, self.static_seq_config)
 
     def perform_phase_calibration(self):
+        """perform phase calibration"""
         # run phase cal
         self.write_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG, 0x02)
 
@@ -93,10 +100,12 @@ class VL53L0X(object):
         self.write_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG, self.static_seq_config)
 
     def perform_single_ref_calibration(self, byte):
+        """perform single ref calibration"""
         self.write_byte(VL53L0X_REG_SYSRANGE_START, VL53L0X_REG_SYSRANGE_MODE_START_STOP | byte)
         self.write_byte(VL53L0X_REG_SYSRANGE_START, 0x00)
 
     def ref_calibration_io(self, byte):
+        """read vhv/phase calibration"""
         # read vhv from device
         self.write_byte(0xFF, 0x01)
         self.write_byte(0x00, 0x00)
@@ -109,6 +118,7 @@ class VL53L0X(object):
         self.write_byte(0xFF, 0x00)
 
     def perform_ref_spad_management(self):
+        """perform ref spad management"""
         self.write_byte(0xFF, 0x01)
         self.write_byte(VL53L0X_REG_DYNAMIC_SPAD_REF_EN_START_OFFSET, 0x00)
         self.write_byte(VL53L0X_REG_DYNAMIC_SPAD_NUM_REQUESTED_REF_SPAD, 0x2C)
@@ -120,6 +130,7 @@ class VL53L0X(object):
         self.perform_ref_signal_measurement()
 
     def perform_ref_signal_measurement(self):
+        """perform ref signal measurement"""
         self.write_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG, 0xC0)
 
         self.perform_single_ranging_measurement()
@@ -131,13 +142,16 @@ class VL53L0X(object):
         self.write_byte(VL53L0X_REG_SYSTEM_SEQUENCE_CONFIG, self.static_seq_config)
 
     def perform_single_ranging_measurement(self):
+        """perform single ranging measurement and get its data"""
         self.perform_single_measurement()
         self.get_ranging_measurement_data()
 
     def perform_single_measurement(self):
+        """perform single measurement"""
         self.start_measurement()
 
     def start_measurement(self):
+        """start measurement"""
         self.write_byte(0x80, 0x01)
         self.write_byte(0xFF, 0x01)
         self.write_byte(0x00, 0x00)
@@ -152,6 +166,7 @@ class VL53L0X(object):
         self.write_byte(VL53L0X_REG_SYSRANGE_START, 0x01)
 
     def get_ranging_measurement_data(self):
+        """get ranging measurement data"""
         raw_data = self.read_block(0x14)
 
         range_millimeter = make_uint16(raw_data[11], raw_data[10])
@@ -167,14 +182,17 @@ class VL53L0X(object):
         self.measurement = range_millimeter
 
     def write_byte(self, reg, data):
+        """write byte data to a given register address"""
         self.bus.write_byte_data(self.address, reg, data)
 
     def read_byte(self, reg):
+        """read byte data from a given register address"""
         read = self.bus.read_byte_data(self.address, reg)
 
         return read
 
     def read_block(self, reg):
+        """write i2c block data to a given register address"""
         read = self.bus.read_i2c_block_data(self.address, reg)
 
         return read
--- vl53l0x/utils.py
+++ vl53l0x/utils.py
@@ -3,4 +3,5 @@
 #
 
 def make_uint16(lsb, msb):
+    """make a meaningful uint16 out of LSB and MSB"""
     return (msb << 8) + lsb
